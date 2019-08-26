e75f8872a37c7227efc1b426366c6eecddc4302a
Luis Concepcion Bido <laconcepcionbido@gmail.com>
2019-08-26 10:27:48 -0400

    added service, entities, and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "blog", component: BlogComponent},
+  {path: "sign-in", component: SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ApiService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BlogComponent } from './blog/blog.component';
+//Classes
+import { Blogger } from './Entities/blogger';
+import { User } from './Entities/user';
+import { Blog } from './Entities/blog';
+import { Profile } from './Entities/profile';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavComponent } from './nav/nav.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    BlogComponent,
+    NavComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'})//withConfig: remove warning message when using formcontrolname and ngModel
   ],
-  providers: [],
+  providers: [ApiService, User, Blogger, Blog, Profile ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
